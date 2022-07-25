9ba3c76e15ad8f6304e7f8f0a316cdc836596913
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-24 22:11:28 -0300

    fix: env.local

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import Stripe from 'stripe';
 import { version } from '../../package.json';
 
+const key = 'sk_test_51LOunFKbaj0BGkLzDYj3biZ8Zu94Wn7539jtwXYWKORyU7FHigJKLixHtlKKAh9dsc2Ysz3qoWFtSsiM7HSmm1Fz000yq2PeE6'
+
 export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY,
+    key,
     {
         apiVersion: '2020-08-27',
         appInfo: {
