bd3e7070995c09c0b428eb0c2fa8a48de645b458
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-27 11:55:29 -0300

    Configurando o faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,29 +1,28 @@
 import { FaGithub } from "react-icons/fa";
-import { FiX} from 'react-icons/fi';
-import { signIn, signOut ,useSession } from 'next-auth/react';
-
-
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-
   const { data: session } = useSession();
- console.log(session)
+  console.log(session);
   return session ? (
-    <button 
-    className={styles.SingInButton} 
-    type="button"
-    onClick={() => signOut()}>
+    <button className={styles.SingInButton} type="button">
       <FaGithub color="#04d361" />
       {session.user.name}
-      <FiX color="#737380" className={styles.closeIcon}/>
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button 
-    className={styles.SingInButton} 
-    type="button"
-    onClick={() => signIn('github')}>
+    <button
+      className={styles.SingInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
