80e5ff56c15d7b34657f03113413da5d69ec2bd2
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-27 16:04:44 -0300

    fix .env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,10 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
-const github_id = '9d3fca6a9cbcfa1e4607';
-const github_secret = '9af800325c6108da6af3d3c6e972111f8ac9669d';
-
 export default NextAuth({
   providers: [
   GithubProvider({
-    clientId: github_id,
-    clientSecret: github_secret,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
     authorization: {
        params: { scope: "openid your_custom_scope" } 
       }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,10 +2,11 @@ import 'dotenv/config';
 import Stripe from 'stripe';
 import { version } from '../../package.json';
 
-const key = 'sk_test_51LOunFKbaj0BGkLzDYj3biZ8Zu94Wn7539jtwXYWKORyU7FHigJKLixHtlKKAh9dsc2Ysz3qoWFtSsiM7HSmm1Fz000yq2PeE6'
+// const key = 'sk_test_51LOunFKbaj0BGkLzDYj3biZ8Zu94Wn7539jtwXYWKORyU7FHigJKLixHtlKKAh9dsc2Ysz3qoWFtSsiM7HSmm1Fz000yq2PeE6'
 
 export const stripe = new Stripe(
-    key,
+    //key,
+    process.env.STRIPE_API_KEY,
     {
         apiVersion: '2020-08-27',
         appInfo: {
