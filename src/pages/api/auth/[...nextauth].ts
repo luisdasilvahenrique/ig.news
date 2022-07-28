e1f7048c9586deba0c6d614ee7862a1c7fdee7a4
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-28 16:38:58 -0300

    salvando user no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,34 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   providers: [
-  GithubProvider({
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    authorization: {
-       params: { scope: "openid your_custom_scope" } 
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: { scope: "openid your_custom_scope" },
+      },
+    }),
+  ],
+  // jwt: {
+  //   singingKey: process.env.SINGING_KEY,
+  // },
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
       }
-  }),
-]
-})
+    },
+  },
+});
