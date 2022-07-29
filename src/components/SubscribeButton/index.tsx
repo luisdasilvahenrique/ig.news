5d4f424e6486e48fedf0a1b4539e6e258b66b18a
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-07-29 20:03:08 -0300

    :construction: gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,30 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
     priceId: string;
 }
 
+
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession();
+    
+    function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+        
+    }
+
     return (
         <button
         type="button"
-        className={styles.subscribeButton}>
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     );
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,6 +20,7 @@ export default NextAuth({
   },
   callbacks: {
     async signIn({ user }) {
+      
       const { email } = user;
 
       try {
