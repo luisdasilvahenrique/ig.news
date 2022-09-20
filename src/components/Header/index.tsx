34d94dec8c223efc0ab39a21ca9a6414b552c284
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-09-20 20:20:52 -0300

    Navegação no Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -8,8 +10,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="Logo ig.news" />
                 <nav>
-                    <a href="#"className={styles.active}>Home</a>
-                    <a href="#">Posts</a>
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/Posts">
+                        <a>Posts</a>
+                    </Link>
                 </nav>
 
                 <SignInButton />
