573aa63d7974a05d2e28af05649a84ccdaffffe9
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2023-02-01 18:33:56 -0300

    testing getStaticProps

--- tests/pages/Home.spec.tsx
+++ tests/pages/Home.spec.tsx
@@ -1,21 +1,47 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../src/pages";
+import Home, { getStaticProps } from "../../src/pages";
+import { stripe } from "../../src/services/stripe";
+import { mocked } from "jest-mock";
 
-jest.mock('next/router', () => ({
-    useRouter: jest.fn(),
-}))
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
 
-jest.mock('next-auth/react', () => {
-    return {
-        useSession: () => [null, false]
-    }
-})
+jest.mock("next-auth/react", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+
+jest.mock("../../src/services/stripe");
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    jest.mock('next/router', () => require('next-router-mock'));
+    jest.mock("next/router", () => require("next-router-mock"));
     render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
 
-    expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("load initial data", async () => {
+    const retriveStripeMocked = mocked(stripe.prices.retrieve);
+
+    retriveStripeMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
   });
 });
